44bb6b5a29d8ef2de6359bc52be4726266d53f2e
geelato-b <81722679+geelato-b@users.noreply.github.com>
2021-04-01 09:15:03 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,443 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgriComercio</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+</head>
+<body>
+
+    <header id="header">
+
+        <div class="right">
+            <img clas ="logo" src="img/logo1.png" alt="" width="70px" height="70px">
+            <div class="fas fa-bars" id="bars"></div>
+        </div>
+        
+        <div class="left">
+            <div class="search-container">
+                <label for="search" class="fas fa-search"></label>
+                <input type="search" placeholder="" id="search">
+            </div>
+
+                <a href="profile.php"><div class="fas fa-user"></div></a>
+                <a href="cart.php"><div class="fas fa-shopping-cart"></div></a>
+
+        </div>
+
+        <nav class="navbar">
+        <ul>
+        <li><a href="sign_in.php">Sign in</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="product.php">Product</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="about.php">About</a></li>
+        </ul>
+    </nav>
+
+    </header>
+
+    <section id="home">
+       
+        <div class="slide">
+            <div class="row">
+                <div class="col-2">
+                    <div class="content text-center text-md-left pl-md-3 ml-md-3">
+                        
+                        <h1>AgriComercio - Change The Way</h1>
+                        <h1>You Trade.</h1>
+                        <h2>Right out of the Farm!</h2>
+                        <h2>Reap Fresh, Eat Fresh</h2>
+                
+                             <a href="sign_in.php"><button class="btn">Sign In &#8594;</button></a>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <img class="img" src="img/Ag1.png" alt="">
+                </div>
+            </div>
+        </div>
+    
+       
+    </section>
+
+
+    <div class="caption">
+        <h2>"Finest Product, Finest Agriculture"</h2>
+        <a href="product.php" class="btn-promo">Shop Now 🛍</a>
+    
+    </div> 
+
+
+<section id="product-category">
+    <div class="heading-two">Category</div>
+    <div class="category-cont">
+        <div class="category-slider">
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="img/c1.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="fruits.php">Fruits</a>
+                </div>
+                
+            </div>
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="img/c2.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="vegetable.php">Vegetables</a>
+                </div>
+            </div>
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="img/c3.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="plants.php">Plants</a>
+                </div>
+            </div>
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="img/c4.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="crops.php">Crops</a>
+                </div>
+            </div>
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="img/c5.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="ferti.php">Fertilizers</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+    <section id="promo">
+
+        <div class="row">
+        <div class="col-2">
+            <img src="img/p1.jpg" alt="">
+        </div>
+        <div class="col-2">
+            <img src="img/p2.jpg" alt="">
+        </div>
+    </div>
+</div>
+
+    </section>
+
+   
+
+    <section id="product">
+
+<h1 class="heading">Feature Products</h1>
+<div class="product-container">
+<div class="product-slider">
+    <div class="product-card item">
+        <div class="image">
+            <img src="img/1.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>Fruit Bundle</h3>
+            <div class="rating">
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+            </div>
+            <div class="price"><p><strike>₱299</strike> - ₱267</p>
+            </div>
+            <button>add to cart</button>
+            
+        </div>
+
+    </div>
+
+    <div class="product-card item">
+        <div class="image">
+            <img src="img/2.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>lettuce</h3>
+            <div class="rating">
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+            </div>
+            <div class="price"><p><strike>₱299</strike> - ₱267</p>
+            </div>
+            <button>add to cart</button>
+        </div>
+
+
+    </div>
+
+    <div class="product-card item">
+        <div class="image">
+            <img src="img/3.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>Rice/Sack</h3>
+            <div class="rating">
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+            </div>
+            <div class="price"><p><strike>₱299</strike> - ₱267</p>
+            </div>
+            <button>add to cart</button>
+        </div>
+
+    </div>
+
+    <div class="product-card item">
+        <div class="image">
+            <img src="img/4.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>Sweet Potato</h3>
+            <div class="rating">
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+            </div>
+            <div class="price"><p><strike>₱299</strike> - ₱267</p>
+            </div>
+           <button>add to cart</button>
+        </div>
+
+    </div>
+
+</div>
+</div>
+
+
+
+<div class="product-container">
+    <div class="product-slider">
+        <div class="product-card item">
+            <div class="image">
+                <img src="img/5.jpg" alt="">
+            </div>
+    
+            <div class="content">
+                <h3>Pineapple</h3>
+                <div class="rating">
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                </div>
+                <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                </div>
+                <button>add to cart</button>
+                
+            </div>
+    
+        </div>
+    
+        <div class="product-card item">
+            <div class="image">
+                <img src="img/6.jpg" alt="">
+            </div>
+    
+            <div class="content">
+                <h3>Spinach</h3>
+                <div class="rating">
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                </div>
+                <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                </div>
+                <button>add to cart</button>
+            </div>
+    
+    
+        </div>
+    
+        <div class="product-card item">
+            <div class="image">
+                <img src="img/7.jpg" alt="">
+            </div>
+    
+            <div class="content">
+                <h3>Coffee Beans</h3>
+                <div class="rating">
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                </div>
+                <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                </div>
+                <button>add to cart</button>
+            </div>
+    
+        </div>
+    
+        <div class="product-card item">
+            <div class="image">
+                <img src="img/8.jpg" alt="">
+            </div>
+    
+            <div class="content">
+                <h3>Coconut</h3>
+                <div class="rating">
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                </div>
+                <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                </div>
+                <button>add to cart</button>
+            </div>
+    
+        </div>
+    
+    </div>
+    </div>
+
+    </section>
+    
+
+    <section id="home-two">
+       
+       <div class="slide">
+           <div class="row">
+               <div class="col-2">
+                   <div class="content text-center text-md-left pl-md-3 ml-md-3">
+                   <img class="img" src="img/cf4.png" alt="">
+                   </div>
+               </div>
+               <div class="col-2">
+               <h1>The best grain, the finest roast, the most powerful flavor.</h1>
+               <a href="product.php"><button class="btttn">Shop Now</button></a>
+
+               </div>
+           </div>
+       </div>
+   
+      
+   </section>
+
+
+   <section id="review">
+   <div class="heading-two">Our Customer Review</div>
+
+   <div class="box-container">
+   <div class="review-slider">
+   <div class="box">
+       <div class="comment">
+            <p><i class="fas fa-quote-left"></i>I have been buying things from agricomercio for a couple of weeks What I like about them is that you can really save money especially during special days <i class="fas fa-quote-right"></i></p>
+            <div class="rating">
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+            </div>
+        <h3>---Lee Jieun</h3>
+       </div>
+  
+   </div>
+   </div>
+   </div>
+
+   <div class="box-container">
+   <div class="review-slider">
+   <div class="box">
+       <div class="comment">
+            <p><i class="fas fa-quote-left"></i>Its convinient and easy to use.<i class="fas fa-quote-right"></i></p>
+            <div class="rating">
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+            </div>
+        <h3>---Kim Taehyung</h3>
+       </div>
+  
+   </div>
+   </div>
+   </div>
+
+   <div class="box-container">
+   <div class="review-slider">
+   <div class="box">
+       <div class="comment">
+            <p><i class="fas fa-quote-left"></i>The products are fresh and organic<i class="fas fa-quote-right"></i></p>
+            <div class="rating">
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+                    <i class="fas fa-heart"></i>
+            </div>
+        <h3>---Jeon Jungkook</h3>
+       </div>
+  
+   </div>
+   </div>
+   </div>
+   
+   </section>
+
+
+<section id="footer">
+   <div class="footer-content">
+   <img clas ="logo" src="img/logo1.png" alt="" width="70px" height="70px">
+       <h3>AgriComercio</h3>
+       <p>Change The Way You Trade</p>
+       <ul class="socials">
+       <li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+       <li><a href=""><i class="fab fa-twitter-square"></i></a></li>
+       <li><a href=""><i class="fab fa-instagram-square"></i></a></li>
+       <li><a href=""><i class="fab fa-google-plus-square"></i></a></li>
+       </ul>
+   </div>
+    <div class="credit text-center">&#169; copyright @ 2021 by ShareQlang</div>
+</section>
+
+
+
+
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/jquery.js"></script> 
+    <script src="js/main.js"></script>
+    
+
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgriComercio</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+</head>
+<body>
+    
+
+<header id="header">
+
+<div class="right">
+    <img clas ="logo" src="img/logo1.png" alt="" width="70px" height="70px">
+    <div class="fas fa-bars" id="bars"></div>
+</div>
+
+<div class="left">
+    <div class="search-container">
+        <label for="search" class="fas fa-search"></label>
+        <input type="search" placeholder="" id="search">
+    </div>
+
+        <a href="profile.php"><div class="fas fa-user"></div></a>
+        <a href="cart.php"><div class="fas fa-shopping-cart"></div></a>
+
+</div>
+
+<nav class="navbar">
+<ul>
+<li><a href="sign_in.php">Sign in</a></li>
+<li><a href="index.php">Home</a></li>
+<li><a href="product.php">Product</a></li>
+<li><a href="services.php">Services</a></li>
+<li><a href="about.php">About</a></li>
+</ul>
+</nav>
+
+</header>
+
+<body>
+
+<section id="services">
+       
+<div class="slide">
+            <div class="row">
+                <div class="col-2">
+                    <div class="content text-center text-md-left pl-md-3 ml-md-3">
+                        
+                        <h1>AgriComercio - Change The Way</h1>
+                        <h1>You Trade.</h1>
+                        <h2>AgriComercio Services</h2>
+                        <h2>We have the solutions for your Agri-Needs</h2>
+            
+                
+                             <a href="sign_in.php"><button class="btn">Sign In &#8594;</button></a>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <img class="img" src="img/sv3.png" alt="">
+                </div>
+            </div>
+        </div>
+    
+</section> 
+
+
+<section id="services-avail">
+
+<div class="row">
+<h2>We provide Information and Consultation</h2>
+<div class="col-2">
+    <img src="img/info.png" alt="">
+</div>
+<div class="col-2">
+<img src="img/consult.png" alt="">
+</div>
+</div>
+</div>
+
+</section>
+
+
+
+
+
+<section id="promo">
+
+        <div class="row">
+        <div class="col-2">
+            <img src="img/sv4.png" alt="">
+            <img src="img/sv7.png" alt="">
+        </div>
+        <div class="col-2">
+            <img src="img/sv5.png "alt="">
+        </div>
+    </div>
+</div>
+
+    </section>
+
+
+    <section id="caption">
+    <h2>We provide a better way to supply and distribute agri-products</h2>
+        <a href="product.php" class="btn-promo">Shop Now 🛍</a> 
+    </section>
+
+
+    <section id="product">
+<div class="product-container">
+<div class="product-slider">
+    <div class="product-card item">
+        <div class="image">
+            <img src="img/free.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>Free Shipping Promos</h3>
+            
+        </div>
+
+    </div>
+
+
+    <div class="product-card item">
+        <div class="image">
+            <img src="img/f1.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>Discounts</h3>
+        </div>
+
+
+    </div>
+
+
+    <div class="product-card item">
+        <div class="image">
+            <img src="img/f2.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>Online Payment</h3>
+            
+        </div>
+
+    </div>
+
+    
+</div>
+</div>
+</section>
+
+
+
+<section id="footer">
+   <div class="footer-content">
+   <img clas ="logo" src="img/logo1.png" alt="" width="70px" height="70px">
+       <h3>AgriComercio</h3>
+       <p>Change The Way You Trade</p>
+       <ul class="socials">
+       <li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+       <li><a href=""><i class="fab fa-twitter-square"></i></a></li>
+       <li><a href=""><i class="fab fa-instagram-square"></i></a></li>
+       <li><a href=""><i class="fab fa-google-plus-square"></i></a></li>
+       </ul>
+   </div>
+    <div class="credit text-center">&#169; copyright @ 2021 by ShareQlang</div>
+</section>
+
+
+
+
+
+<script src="js/bootstrap.bundle.js"></script>
+    <script src="js/jquery.js"></script> 
+    <script src="js/main.js"></script>
+    
+</body>
+</html>
