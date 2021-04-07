ee17bc67d7fdca8d7747ebadda0c96b6e8b698c1
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-04-07 18:34:17 +0800

    Add files via upload

---
+++ category/vegetable.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>AgriComercio</title>
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/fontawesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    
+<header id="header">
+
+        <div class="right">
+            <img clas ="logo" src="../img/logo1.png" alt="" width="70px" height="70px">
+            
+        </div>
+
+        <div class="nav-bar">
+            <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="../product.php">Product</a></li>
+            <li><a href="../services.php">Services</a></li>
+            <li><a href="../about.php">About</a></li>
+            <li><a href="../sign_in.php">Sign in</a></li>
+            </ul>
+        </div>
+        
+        <div class="left">
+                <a href="../profile.php"><div class="fas fa-user"></div></a>
+                <a href="../cart.php"><div class="fas fa-shopping-cart"></div></a>
+
+        </div>
+
+        
+
+    </header>
+
+
+   
+
+
+    
+    <section id="product">
+
+        <h1 class="heading">Products</h1>
+        <div class="product-container">
+        <div class="product-slider">
+            <div class="product-card item">
+                <div class="image">
+                    <img src="../img/1.png" alt="">
+                </div>
+        
+                <div class="content">
+                    <h3>Upo per kilo</h3>
+                    <div class="rating">
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                    </div>
+                    <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                    </div>
+                    <button >add to cart</button>
+                    
+                </div>
+        
+            </div>
+        
+            <div class="product-card item">
+                <div class="image">
+                    <img src="../img/2.jpg" alt="">
+                </div>
+        
+                <div class="content">
+                    <h3>Lettuce per bundle</h3>
+                    <div class="rating">
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                    </div>
+                    <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                    </div>
+                    <button>add to cart</button>
+                </div>
+        
+        
+            </div>
+        
+            <div class="product-card item">
+                <div class="image">
+                    <img src="../img/3.png" alt="">
+                </div>
+        
+                <div class="content">
+                    <h3>Sitaw per bundle</h3>
+                    <div class="rating">
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                    </div>
+                    <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                    </div>
+                    <button>add to cart</button>
+                </div>
+        
+            </div>
+        
+            <div class="product-card item">
+                <div class="image">
+                    <img src="../img/5.png" alt="">
+                </div>
+        
+                <div class="content">
+                    <h3>Sigarilyas per kilo</h3>
+                    <div class="rating">
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                        <i class="fa fa-star" ></i>
+                    </div>
+                    <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                    </div>
+                    <button>add to cart</button>
+                </div>
+        
+            </div>
+        
+        </div>
+        </div>
+        
+        
+        
+        <div class="product-container">
+            <div class="product-slider">
+                <div class="product-card item">
+                    <div class="image">
+                        <img src="../img/6.jpg" alt="">
+                    </div>
+            
+                    <div class="content">
+                        <h3>Spinach per bundle</h3>
+                        <div class="rating">
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                        </div>
+                        <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                        </div>
+                        <button >add to cart</button>
+                        
+                    </div>
+            
+                </div>
+            
+                <div class="product-card item">
+                    <div class="image">
+                        <img src="../img/6.png" alt="">
+                    </div>
+            
+                    <div class="content">
+                        <h3>Sayote per kilo</h3>
+                        <div class="rating">
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                        </div>
+                        <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                        </div>
+                        <button>add to cart</button>
+                    </div>
+            
+            
+                </div>
+            
+                <div class="product-card item">
+                    <div class="image">
+                        <img src="../img/9.png" alt="">
+                    </div>
+            
+                    <div class="content">
+                        <h3>Banana Blossom</h3>
+                        <div class="rating">
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                        </div>
+                        <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                        </div>
+                        <button>add to cart</button>
+                    </div>
+            
+                </div>
+            
+                <div class="product-card item">
+                    <div class="image">
+                        <img src="../img/10.png" alt="">
+                    </div>
+            
+                    <div class="content">
+                        <h3>Pechay per bundle</h3>
+                        <div class="rating">
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                        </div>
+                        <div class="price"><p><strike>₱299</strike> - ₱267</p>
+                        </div>
+                        <button>add to cart</button>
+                    </div>
+            
+                </div>
+            
+            </div>
+            </div>
+        
+            </section>
+
+
+
+            <section id="product-category">
+    <div class="heading-two">Category</div>
+    <div class="category-cont">
+        <div class="category-slider">
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="../img/c1.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="fruits.php">Fruits</a>
+                </div>
+                
+            </div>
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="../img/c2.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="vegetable.php">Vegetables</a>
+                </div>
+            </div>
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="../img/c3.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="plants.php">Plants</a>
+                </div>
+            </div>
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="../img/c4.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="crops.php">Crops</a>
+                </div>
+            </div>
+            <div class="category-card">
+                <div class="category-img">
+                    <img src="../img/c5.jpg" alt="">
+                </div>
+                <div class="content-cat">
+                <a href="ferti.php">Fertilizers</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+            <div class="page-btn">
+                <a href="vegetable.php">1</a>
+                <a href="vegetable_two.php">2</a> 
+                <a href="vegetable_three.php">3</a>              
+            </div>
+
+
+
+<section id="footer">
+   <div class="footer-content">
+   <img clas ="logo" src="../img/logo1.png" alt="" width="70px" height="70px">
+       <h3>AgriComercio</h3>
+       <p>Change The Way You Trade</p>
+       <ul class="socials">
+       <li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+       <li><a href=""><i class="fab fa-twitter-square"></i></a></li>
+       <li><a href=""><i class="fab fa-instagram-square"></i></a></li>
+       <li><a href=""><i class="fab fa-google-plus-square"></i></a></li>
+       </ul>
+   </div>
+    <div class="credit text-center">&#169; copyright @ 2021 by ShareQlang</div>
+</section>
+
+        
+        
+            <script src="../js/bootstrap.bundle.js"></script>
+            <script src="../js/jquery.js"></script> 
+            <script src="../js/main.js"></script>
+</body>
+</html>
