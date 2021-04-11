bfaa2b48c5cf7dfe1911a0f892e21bed1ef78790
Christine Joyce Precones <77787569+CJPrecones@users.noreply.github.com>
2021-04-11 19:12:31 +0800

    seller product page edits
    add items on seller page

--- seller/product.php
+++ seller/product.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -57,15 +58,60 @@
    
 <section id="review">
   
-   
-   </section>
+<?php
+session_start();
+include_once "../includes/db_conn.php";
+include_once "../includes/function.inc.php";
 
+if(isset($_SESSION['usertype'])){
+    if($_SESSION['usertype'] == 'Seller'){
     
+    $USER_ID = $_SESSION['userid'];
+    
+      
+        $user_info = GetUserDetails($conn, $USER_ID );
+    
+      
+      if(isset($_POST['item_name'])){
+          $p_item_name = htmlentities($_POST['item_name']);
+          $p_item_desc = htmlentities($_POST['item_desc']);
+          $p_item_status = htmlentities($_POST['item_status']);
+          $p_item_price = htmlentities($_POST['item_price']);
+          
+         if( AddItem($conn,$USER_ID,$p_item_name,$p_item_desc,$p_item_status,$p_item_price) !== false) {
+             
+             echo "Item has been added.";
+         }
+          else{
+              echo "Oops!Something's wrong.";
+          }
+      }
+           ?>
+        <form action="seller_page.php" method="post">
+            Item name :  <input type="text" name="item_name">
+            <br>
+            Item Desc :  <input type="text" name="item_desc">
+            <br>
+            Item Status :  <input type="text" name="item_status">
+            <br>
+            Item Price :  <input type="text" name="item_price">
+            <br>
+            <input type="submit">
+        </form>
+        
+    
+    <?php }
+}
+else{
+    header("location: index.php");
+    
+}
+    
+?>
+
+
+</section>
    
-
-
-
-
 
 <section id="footer">
    <div class="footer-content">
