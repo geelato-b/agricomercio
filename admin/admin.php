545750b95529df16317f3b1e679da8f4c4350d27
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-04-14 22:05:28 +0800

    Add files via upload

--- admin/admin.php
+++ admin/admin.php
@@ -1,134 +1,102 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/fontawesome.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgriComercio</title>
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/fontawesome.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-    <link rel="stylesheet" href="style.css" />
-    <title>AgriComercio Admin Dashboard</title>
-  </head>
-  <body id="body">
-    <div class="container">
-      <nav class="navbar">
-        <div class="nav_icon" onclick="toggleSidebar()">
-          <i class="fa fa-bars" aria-hidden="true"></i>
-        </div>
-        <div class="navbar__left">
-          <a class="active_link" href="#">Admin</a>
-        </div>
-        <div class="navbar__right">
-          <a href="#">
-          <input type="text" placeholder="Search..">
-          <button><i class="fa fa-search"></i></button>
-          </a>
-        </div>
-      </nav>
 
-      <main>
-        <div class="main__container">
-          <!-- MAIN TITLE STARTS HERE -->
+</head>
+<body>
+    
 
-          <div class="main__title">
-            <img src="assets/hello.svg" alt="" />
-            <div class="main__greeting">
-              <h1>Hello</h1>
-              <p>Welcome to your admin dashboard</p>
-            </div>
-          </div>
+<header id="header">
 
-          <!-- MAIN TITLE ENDS HERE -->
+<div class="right">
+    <div class="fas fa-bars" id="bars"></div>
 
-          <div class="main__cards">
-            <div class="card">
-              <i
-                class="fa fa-user-o fa-2x text-lightblue"
-                aria-hidden="true"
-              ></i>
-              <div class="card_inner">
-                <a href =" " class="text-primary-p">Total Number of User</a>
-                <span class="font-bold text-title">50</span>
-              </div>
-            </div>
-
-            <div class="card">
-              <i
-                class="fa fa-user-o fa-2x text-lightblue"
-                aria-hidden="true"
-              ></i>
-              <div class="card_inner">
-                <a href ="seller.php" class="text-primary-p">Seller</a>
-                <span class="font-bold text-title">25</span>
-              </div>
-            </div>
-            <div class="card">
-              <i
-                class="fa fa-user-o fa-2x text-lightblue"
-                aria-hidden="true"
-              ></i>
-              <div class="card_inner">
-                <a href = "" class="text-primary-p">Customer</a>
-                <span class="font-bold text-title">25</span>
-              </div>
-            </div>
-
-            <div class="charts__right">
-              <div class="charts__right__title">
-                <div>
-                  <h1>General Report</h1>
-                </div>
-              </div>
-             
-
-              <div class="charts__right__cards">
-                <div class="card1">
-                 <a href=""><h1>Income</h1></a> 
-                  
-                </div>
-
-                <div class="card2">
-                  <a href=""><h1>Sales</h1></a>
-                 
-                </div>
-
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-
-      <div id="sidebar">
-        <div class="sidebar__title">
-          <div class="sidebar__img">
-            <img src="../img/logo1.png" alt="logo" />
-            <h1>AgriComercio</h1>
-          </div>
-          <i
-            onclick="closeSidebar()"
-            class="fa fa-times"
-            id="sidebarIcon"
-            aria-hidden="true"
-          ></i>
+</div>
+<div class="left">
+        <div class="dropdown">
+                <button class="dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+                <div class="fas fa-user"></div>
+                </button>
+                
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    <a href="../sign_in.php"><li><button class="dropdown-item" type="button">Sign In</button></li></a>
+                    <a href="../logout.php"><li><button class="dropdown-item" type="button">Log Out</button></li></a>
+                </ul>
         </div>
 
-        <div class="sidebar__menu">
-          <div class="sidebar__link active_menu_link">
-            <i class="fa fa-home"></i>
-            <a href="#">Dashboard</a>
-          </div>
-          <div class="sidebar__link">
-            <i class="fa fa-wrench"></i>
-            <a href="#">User Management</a>
-          </div>
-          <div class="sidebar__link">
-            <i class="fa fa-question"></i>
-            <a href="#">Requests</a>
-          </div>
+</div>
+</header>
+
+<!-- Side navigation -->
+
+<body>
+<div class="sidenav">
+  <div class="sidenav-title">
+    <div class="sidenav_image">
+      <img src="../img/logo1.png" alt=""width = "90px ">
+      <h1>AgriComercio</h1>
+    </div>
+  </div>
+
+  <a href="admin.php"><i class="fas fa-home"></i> Dashboard</a>
+  <a href="user.php"><i class="fas fa-users"></i> User Management</a>
+  <a href="#"><i class="fas fa-question"></i> Request</a>
+  
+</div>
+
+<main>
+    <div class="row" id=cards>
+      <div class="column">
+        <div class="card1">
+          <a href="user.php">Users</a>
         </div>
       </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-    <script src="script.js"></script>
-  </body>
+      <div class="column">
+        <div class="card2">
+        <a href="seller.php">Seller</a>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card3">
+        <a href="customer.php">Customer</a>
+        </div>
+      </div>
+
+
+      <div class="row" id="cards">
+       <div class="box_cards">
+         <h2>Generated Report</h2>
+            <div class="card4">
+            <a href="">Sales</a>
+            </div>
+
+            <div class="card5">
+            <a href="">Income</a>
+            </div>
+        
+       </div>
+</div>
+      
+     
+</main>
+
+
+
+
+
+<script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/jquery.js"></script> 
+    <script src="js/main.js"></script>
+    
+</body>
 </html>
+
