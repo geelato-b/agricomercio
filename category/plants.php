127f019d60b015e475c7a417efd753f44490eb69
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-04-19 16:00:47 +0800

    Add files via upload

--- category/plants.php
+++ category/plants.php
@@ -1,10 +1,15 @@
+<?php
+session_start();
+include_once "../includes/db_conn.php";
+include_once "../includes/function.inc.php";   
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AgriComercio</title>
 
     <link rel="stylesheet" href="../css/bootstrap.min.css">
@@ -13,8 +18,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 </head>
 <body>
-    
-<header id="header">
+
+    <header id="header">
 
         <div class="right">
             <img clas ="logo" src="../img/logo1.png" alt="" width="70px" height="70px">
@@ -31,91 +36,121 @@
         </div>
         
         <div class="left">
-
         <div class="dropdown">
-            <a href="../cart.php"><div class="fas fa-shopping-cart"></div></a>
+            <a href="cart.php"><div class="fas fa-shopping-cart"></div></a>
                 <button class="dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
                 <div class="fas fa-user"></div>
                 </button>
                 
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    <a href="../customer_page.php"><li><button class="dropdown-item" type="button">Profile</button></li></a>
+                    <a href="customer_page.php"><li><button class="dropdown-item" type="button">Profile</button></li></a>
                     <a href="../form.php"><li><button class="dropdown-item" type="button">Sign Up</button></li></a>
                     <a href="../sign_in.php"><li><button class="dropdown-item" type="button">Sign In</button></li></a>
                     <a href="../logout.php"><li><button class="dropdown-item" type="button">Log Out</button></li></a>
                 </ul>
-
         </div>
 
-        
+
+        </div>   
 
     </header>
 
-
-   
-
-
+<?php    
+  
+ if(isset($_SESSION['usertype'])){
+    if($_SESSION['usertype'] == 'Seller'){
     
-    <section id="product">
+    $USER_ID = $_SESSION['userid'];
+    
+      
+        $user_info = GetUserDetails($conn, $USER_ID );
+      if(isset($_POST['item_name'])){
+          $p_item_name = htmlentities($_POST['item_name']);
+          $p_item_desc = htmlentities($_POST['item_desc']);
+          $p_item_status = htmlentities($_POST['item_status']);
+          $p_item_price = htmlentities($_POST['item_price']);
+          
+         if( AddItem($conn,$USER_ID,$p_item_name,$p_item_desc,$p_item_status,$p_item_price) !== false) {
+             
+             echo "Item has been added.";
+         }
+          else{
+              echo "Oops!Something's wrong.";
+          }
+      }
+           
 
-        <h1 class="heading">Products</h1>
-        <div class="product-container">
+    }
+}
+
+
+?>
+        
+<?php
+        $sql="SELECT i.item_id,
+                    i.item_img,
+                    i.item_name,
+                    i.item_desc, 
+                    i.item_status, 
+                    c.cat_desc, 
+                    i.item_price 
+                    FROM items i 
+                    JOIN category c 
+                    ON i.cat_id = c.cat_id
+                    WHERE c.cat_id= 3;";
+
+  $stmt=mysqli_stmt_init($conn);
+  if (!mysqli_stmt_prepare($stmt, $sql)){
+     echo "Statement Failed.";
+     exit();
+  }
+
+   mysqli_stmt_execute($stmt);
+  //get the results of the executed statement and put it into a variable
+   $resultData = mysqli_stmt_get_result($stmt);
+  //declare a container array.
+   $arr=array();
+   while($row = mysqli_fetch_assoc($resultData)){
+      
+       array_push($arr,$row);
+   }
+      ?>
+      
+<section id="product">
+    <div class="product-container">
+        <?php
+        foreach($arr as $key => $val){
+        ?>
+
         <div class="product-slider">
             <div class="product-card item">
                 <div class="image">
-                    <img src="../img/37.png" alt="">
+                <img src="../img/<?php echo $val['item_img'] ?>" alt="" class="card-img-top">
                 </div>
-        
-                <div class="content">
-                    <h3>Daisy per Pot</h3>
-                    <div class="rating">
-                        <i class="fa fa-star" ></i>
-                        <i class="fa fa-star" ></i>
-                        <i class="fa fa-star" ></i>
-                        <i class="fa fa-star" ></i>
-                        <i class="fa fa-star" ></i>
+
+                    <div class="content">
+                        <h3><?php echo $val['item_name'] ?></h3>
+                        <h4><?php echo $val['item_desc'] ?></h4>
+                        <div class="price"><p><?php echo number_format($val['item_price'],2)  ?></p>
+                        </div>
+                        <label for="Quantity">Quantity</label>
+                        <input type="number" value = "1" min="1" max="10">
+                        <button class = "btn-addcart">
+                        <a href="orderform.php?itemid=" <?php $val['item_id']?>><i class="fas fa-shopping-cart"></i></a>
+                        </button>
+                   
                     </div>
-                    <div class="price"><p><strike>₱299</strike> - ₱267</p>
-                    </div>
-                    <button >add to cart</button>
-                    
-                </div>
-        
             </div>
-        
-            <div class="product-card item">
-                <div class="image">
-                    <img src="../img/36.png" alt="">
-                </div>
-        
-                <div class="content">
-                    <h3>White Angel per Pot</h3>
-                    <div class="rating">
-                        <i class="fa fa-star" ></i>
-                        <i class="fa fa-star" ></i>
-                        <i class="fa fa-star" ></i>
-                        <i class="fa fa-star" ></i>
-                        <i class="fa fa-star" ></i>
-                    </div>
-                    <div class="price"><p><strike>₱299</strike> - ₱267</p>
-                    </div>
-                    <button>add to cart</button>
-                </div>
-        
-        
-            </div>
-        
         </div>
-        </div>
-        
-        
-        
- 
+
+                    <?php
+                        }
+                        ?>
+    </div>
 </section>
 
 
-
-    <section id="product-category">
+<section id="product-category">
     <div class="heading-two">Category</div>
     <div class="category-cont">
         <div class="category-slider">
@@ -164,10 +199,8 @@
     </div>
 
 </section>
-
-
-
-<section id="footer">
+      
+    <section id="footer">
    <div class="footer-content">
    <img clas ="logo" src="../img/logo1.png" alt="" width="70px" height="70px">
        <h3>AgriComercio</h3>
@@ -180,10 +213,7 @@
        </ul>
    </div>
     <div class="credit text-center">&#169; copyright @ 2021 by ShareQlang</div>
-</section>
-
-        
-        
+</section>       
             <script src="../js/bootstrap.bundle.js"></script>
             <script src="../js/jquery.js"></script> 
             <script src="../js/main.js"></script>
