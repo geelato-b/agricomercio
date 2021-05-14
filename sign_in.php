0ad93d9eca5054727f23ccb15a2a35d0b68305c3
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-05-14 14:11:20 +0800

    Update sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -2,9 +2,6 @@
 include_once ('includes/db_conn.php');
 include_once ('includes/function.inc.php');
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +10,7 @@ include_once ('includes/function.inc.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AgriComercio</title>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/fontawesome.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
@@ -21,50 +18,106 @@ include_once ('includes/function.inc.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 </head>
 <body>
-    <header id="header">
-    <div class="right">
-        <img clas ="logo" src="img/logo1.png" alt="" width="70px" height="70px">  
-    </div>
-    <div class="nav-bar">
-            <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="product.php">Product</a></li>
-            <li><a href="services.php">Services</a></li>
-            <li><a href="about.php">About</a></li>
-            </ul>
-        </div>
+<header id="header">
+    <nav class="navbar navbar-expand-lg ">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+        <div class="right">
+        <img clas ="logo" src="img/logo2.png" alt="" width="100px" height="100px">
+                
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span   class="navbar-toggler-icon"><i style= "color:black; " class="fas fa-bars"></i></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="product.php">Products</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="services.php">Services</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="About.php">About Us</a>
+            </li>
+            
+        </ul>
 
-    <div class="left">
+        <div class="dropdown">
+            <a href="cart.php"><div class="fas fa-shopping-cart"></div>
+                <?php 
+                            $sql_cart_count = "SELECT COUNT(*) cartcount FROM `cart` WHERE status = 'P' AND user_id = ?;";
+                            $stmt=mysqli_stmt_init($conn);
+        
+                        if (!mysqli_stmt_prepare($stmt, $sql_cart_count)){
+                            header("location: index.php?error=stmtfailed");
+                            exit();
+                        }
+                            mysqli_stmt_bind_param($stmt, "s" ,$_SESSION['userid']);
+                            mysqli_stmt_execute($stmt);
 
-    <div class="dropdown">
+                            $resultData = mysqli_stmt_get_result($stmt);
+
+                            if($row = mysqli_fetch_assoc($resultData)){ ?>
+                                <span class="badge bg-danger"><?php echo $row['cartcount']; ?></span>
+                            <?php }
+                        
+                            ?>
+                
+                </a>
+                
                 <button class="dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
                 <div class="fas fa-user"></div>
                 </button>
                 
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    <a href="customer_page.php"><li><button class="dropdown-item" type="button">Profile</button></li></a>
-                    <a href="logout.php"><li><button class="dropdown-item" type="button">Log Out</button></li></a>
+                
+                    <?php
+                    if(isset($status_logged_in)){
+                            switch($status_logged_in['usertype']){
+                                case 'Customer':
+                            ?>
+                            <a href="customer_page.php"><li><button class="dropdown-item" type="button">Profile</button></li></a>
+                            <a href="logout.php"><li><button class="dropdown-item" type="button">Log Out</button></li></a>
+                    <?php        break;
+                                case 'Admin': 
+                                header("location: admin/admin.php");
+                                    break;
+                                case 'Seller':
+                                header("location: seller/index.php");
+                                break;
+                            }
+                    }
+                    else{ ?>
+                        <a href="form.php"><li><button class="dropdown-item" type="button">Sign Up</button></li></a>
+                        <a href="sign_in.php"><li><button class="dropdown-item" type="button">Sign In</button></li></a>
+                    <?php }
+                    ?>
+                
                 </ul>
+            </div>
         </div>
-
-
+        
+        </div>
     </div>
-
-
+    </nav>
 </header>
-
-
 <section id="sign-in">  
     <div class="slide">
         <div class="row">
             <div class="col">
+            
             <img class="sigin_img" src="img/bg.svg" alt="">
             </div>
-            
             <div class="col">
+            
                 <div class="form-box">
                     <div class="button-box">
-                        <div id="btn"></div>
+                    
                         <div class="login">
                             <h3>Log In</h3>
                         </div>
@@ -105,11 +158,13 @@ include_once ('includes/function.inc.php');
    </div>
     <div class="credit text-center">&#169; copyright @ 2021 by ShareQlang</div>
 </section>
-
-
-    <script src="js/bootstrap.bundle.js"></script>
+    
+   
+<script src="js/bootstrap.bundle.js"></script>
     <script src="js/jquery.js"></script> 
-    <script src="js/main.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+
     
 </body>
 </html>
