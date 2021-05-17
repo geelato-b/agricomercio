8d80ec1c52bb79c40987b281c78085d4c62d334e
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-05-17 19:33:58 +0800

    Add files via upload

--- includes/shipperorderupdate.php
+++ includes/shipperorderupdate.php
@@ -6,14 +6,16 @@ if(isset($_POST['order_id'])){
     
          $sql_upd = "UPDATE `orders`
                         SET  tracking_order_status = ?
-                    WHERE order_id = ?;";
+                    WHERE order_id = ?
+                    AND status = 'C';";
+
         $stmt_upd = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt_upd, $sql_upd)){
-        header("location: ../index.php?error=8"); //update failed
+        header("location: ../shipper/index.php?error=8"); //update failed
         exit();
         }
         mysqli_stmt_bind_param($stmt_upd,"ss",$new_order_status, $order_id);
         mysqli_stmt_execute($stmt_upd);
-        header("location: ../receiveorder.php?success_update=1");
+        header("location: ../shipper/index.php?success_update=1");
         
     }
